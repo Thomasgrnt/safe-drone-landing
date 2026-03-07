f53eb39f23b75249564820e1f510bc19c96050ba
Thomasgrnt <115085294+Thomasgrnt@users.noreply.github.com>
2026-03-07 20:25:50 +0100

    Fix project path for best model weights
    Updated project path for model weights output.

--- train.py
+++ train.py
@@ -2,8 +2,10 @@ from pathlib import Path
 from ultralytics import YOLO
 
 DATA_YAML = "landingpad.yaml"
-MODEL_NAME = "yolo11n.pt"   # léger et adapté à Colab
-PROJECT = "runs/detect"
+MODEL_NAME = "yolo11n.pt"
+
+# IMPORTANT
+PROJECT = "runs"
 RUN_NAME = "landingpad_model"
 
 IMG_SIZE = 640
@@ -31,7 +33,7 @@ def main():
     )
 
     print("\nTraining finished.")
-    print(f"Best model should be here: {PROJECT}/{RUN_NAME}/weights/best.pt")
+    print(f"Best model should be here: {PROJECT}/detect/{RUN_NAME}/weights/best.pt")
 
 if __name__ == "__main__":
     main()
